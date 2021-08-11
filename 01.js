f5ebba14de9bb0e692dcf163b8413bdbe0e7c389
Cangu <xmutizabal@gmail.com>
2021-08-11 00:14:13 -0400

    agegados botones modifican edad, vue

--- 01.js
+++ 01.js
@@ -3,17 +3,29 @@ const app = new Vue({
     data: {
         titulo: 'Ranking Zona Norte',
         corredores: [
-            {nombre:"Nino Meza", posicion: 1, edad: 40},
+            {nombre:"Nino Meza", posicion: 1, edad: 42},
             {nombre:"Ismael Porras", posicion: 2, edad: 34},
             {nombre:"Xavier Mutizabal", posicion: 3, edad: 35},
             {nombre:"Felipe Santander", posicion: 4, edad: 18},
             {nombre:"Cristian Perez", posicion: 5, edad: 28},
         ],
-        nuevoCorredor:''
+        nuevoCorredor:'',
+        total: 0
     },
     methods:{
         agregarCorredor () {
             this.corredores.push({nombre: this.nuevoCorredor, posicion: 6, edad: 22});
+            this.nuevoCorredor="";
+        }
+    },
+    computed:{
+        sumarEdades(){
+            this.total = 0;
+            for(i of this.corredores){
+                this.total = this.total + i.edad;
+            }
+            return this.total;
         }
     }
+
 })
