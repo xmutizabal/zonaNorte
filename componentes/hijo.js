2db6fd7c97ef4fe84ae99484870ece2d5abb848a
Cangu <xmutizabal@gmail.com>
2021-08-20 23:39:23 -0400

    se agregan estilos bootstrap, y comunicacion entre componentes

---
+++ componentes/hijo.js
@@ -0,0 +1,14 @@
+Vue.component('hijo',{
+
+
+    template:
+    
+    `
+    <div class="p-5 bg-success">
+        <h4> Corredores </h4>
+    </div>
+       
+    
+    `
+
+})
---
+++ componentes/padre.js
@@ -0,0 +1,15 @@
+Vue.component('padre',{
+
+
+    template:
+    
+    `
+    <div class="p-2 bg-dark text-white">
+        <h2> Corredores </h2>
+        <hijo></hijo>
+    </div>
+       
+    
+    `
+
+})
